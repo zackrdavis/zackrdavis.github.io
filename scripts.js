532570efe78e6ee9f50449df8681edb1686ad718
zackrdavis <zackrdavis@gmail.com>
2018-04-30 18:32:53 -0400

    fix scroling, add 2 projects

--- scripts.js
+++ scripts.js
@@ -1,35 +1,43 @@
+var bodyScroll = 0;
+var matched = false;
+
 function matchHeightToWidth() {
   if($('body').height() !== $('ul').width() - ($(window).width() - $(window).height())) {
     $('body').height($('ul').width() - ($(window).width() - $(window).height()));
   }
 }
 
-function scrollerMatchBody() {
+// set where vertical scroll should jump to to match horz position
+function setBodyScroll() {
+  //do nothing on mobile
   if($(window).width() <= 580) {
     return;
   }
 
-  matchHeightToWidth();
-
-  $('.scroll-container').scrollLeft($(window).scrollTop());
+  bodyScroll = $('.scroll-container').scrollLeft();
+  matched = false;
 }
 
-function bodyMatchScroller() {
+
+function scrollerMatchBody() {
+  //do nothing on mobile
   if($(window).width() <= 580) {
     return;
   }
 
-  matchHeightToWidth();
-
-  if($('.scroll-container').scrollLeft() !== $(window).scrollTop()) {
-    $(window).scrollTop($('.scroll-container').scrollLeft())
+  if (!matched) {
+    $(window).scrollTop(bodyScroll);
   }
+
+  $('.scroll-container').scrollLeft($(window).scrollTop());
+
+  matched = true;
 }
 
 $(document).ready(function() {
   scrollerMatchBody();
+  matchHeightToWidth();
 })
 
 $(window).on('scroll', scrollerMatchBody);
-
-$(window).on('mousewheel', bodyMatchScroller);
+$('.scroll-container').on('scroll', setBodyScroll);
