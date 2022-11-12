64b72e5be11b434772790a421d989f147b3f903d
Zack Davis <zackrdavis@gmail.com>
2022-11-11 20:54:54 -0800

    prettier scripts and styles

--- scripts.js
+++ scripts.js
@@ -2,26 +2,30 @@ var bodyScroll = 0;
 var matched = false;
 
 function matchHeightToWidth() {
-  if($('body').height() !== $('ul').width() - ($(window).width() - $(window).height())) {
-    $('body').height($('ul').width() - ($(window).width() - $(window).height()));
+  if (
+    $("body").height() !==
+    $("ul").width() - ($(window).width() - $(window).height())
+  ) {
+    $("body").height(
+      $("ul").width() - ($(window).width() - $(window).height())
+    );
   }
 }
 
 // set where vertical scroll should jump to to match horz position
 function setBodyScroll() {
   //do nothing on mobile
-  if($(window).width() <= 580) {
+  if ($(window).width() <= 580) {
     return;
   }
 
-  bodyScroll = $('.scroll-container').scrollLeft();
+  bodyScroll = $(".scroll-container").scrollLeft();
   matched = false;
 }
 
-
 function scrollerMatchBody() {
   //do nothing on mobile
-  if($(window).width() <= 580) {
+  if ($(window).width() <= 580) {
     return;
   }
 
@@ -29,15 +33,15 @@ function scrollerMatchBody() {
     $(window).scrollTop(bodyScroll);
   }
 
-  $('.scroll-container').scrollLeft($(window).scrollTop());
+  $(".scroll-container").scrollLeft($(window).scrollTop());
 
   matched = true;
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   scrollerMatchBody();
   matchHeightToWidth();
-})
+});
 
-$(window).on('scroll', scrollerMatchBody);
-$('.scroll-container').on('scroll', setBodyScroll);
+$(window).on("scroll", scrollerMatchBody);
+$(".scroll-container").on("scroll", setBodyScroll);
