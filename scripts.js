34f560bbc734177d0a0bb7b45e0864da7c656c10
Zack Davis <zackrdavis@gmail.com>
2022-11-13 20:59:58 -0800

    rewrite in all vanilla JS

--- scripts.js
+++ scripts.js
@@ -1,3 +1,55 @@
+/** Force window's Y scroll to match xScrollContainer's X scroll */
+const matchXYScrollHeight = () => {
+  const winWidth = window.innerWidth;
+  const winHeight = window.innerHeight;
+  const contentWidth = xScrollContent.offsetWidth;
+
+  // new body height approximates content width
+  // adjusted for difference between window width/height
+  const newHeight = contentWidth - (winWidth - winHeight);
+
+  // set the style
+  body.style.height = newHeight + "px";
+};
+
+/** decide target y-scroll when x-scrolling */
+const setBodyScroll = () => {
+  const winWidth = window.innerWidth;
+
+  //do nothing on mobile
+  if (winWidth < 580) return false;
+
+  const scrollLeft = xScrollContainer.scrollLeft;
+
+  bodyScroll = scrollLeft;
+
+  window.scrollTo = bodyScroll;
+
+  matched = false;
+};
+
+const scrollerMatchBody = () => {
+  const scrollHeight = body.scrollHeight;
+  const scrollTop = window.scrollY;
+  const winHeight = window.innerHeight;
+  const winWidth = window.innerWidth;
+
+  const maxScroll = scrollHeight - winHeight;
+
+  //do nothing on mobile
+  if (winWidth <= 580 || scrollTop >= maxScroll) {
+    return;
+  }
+
+  if (!matched) {
+    window.scrollTop = bodyScroll;
+  }
+
+  xScrollContainer.scrollLeft = scrollTop;
+
+  matched = true;
+};
+
 var bodyScroll = 0;
 var matched = false;
 
@@ -5,52 +57,8 @@ const body = document.body;
 const xScrollContainer = document.getElementById("x-scroll-container");
 const xScrollContent = document.getElementById("x-scroll-content");
 
-/** Force window's Y scroll to match xScrollContainer's X scroll */
-function matchXYScrollHeight() {
-  const winWidth = window.innerWidth;
-  const winHeight = window.innerHeight;
-  const contentWidth = xScrollContent.offsetWidth;
+scrollerMatchBody();
+matchXYScrollHeight();
 
-  // new body height approximates content width
-  // adjusted for differeve between window width/height
-  const newHeight = contentWidth - (winWidth - winHeight);
-
-  // set the style
-  body.style.height = newHeight + "px";
-}
-
-// set where vertical scroll should jump to to match horz position
-function setBodyScroll() {
-  //do nothing on mobile
-  if (window.innerWidth <= 580) {
-    return;
-  }
-
-  bodyScroll = $("#x-scroll-container").scrollLeft();
-  matched = false;
-}
-
-function scrollerMatchBody() {
-  const maxScroll = $("body").get(0).scrollHeight - $(window).height();
-
-  //do nothing on mobile
-  if ($(window).width() <= 580 || $(window).scrollTop() >= maxScroll) {
-    return;
-  }
-
-  if (!matched) {
-    $(window).scrollTop(bodyScroll);
-  }
-
-  $("#x-scroll-container").scrollLeft($(window).scrollTop());
-
-  matched = true;
-}
-
-$(document).ready(function () {
-  scrollerMatchBody();
-  matchXYScrollHeight();
-});
-
-$(window).on("scroll", scrollerMatchBody);
-$("#x-scroll-container").on("scroll", setBodyScroll);
+window.addEventListener("scroll", scrollerMatchBody);
+xScrollContainer.addEventListener("scroll", setBodyScroll);
